ace03648283a4a528c276ea8d1c6aeb22dfdca02
brickly26 <sabburak26@gmail.com>
2022-09-12 12:38:02 -0400

    added transform and transiton times

--- Day8/script.js
+++ Day8/script.js
@@ -5,6 +5,7 @@ console.log(labels)
 labels.forEach((label) => {
   label.innerHTML = label.innerText
     .split("")
-    .map((letter, idx) => `<span>${letter}</span>`)
+    .map((letter, idx) => `<span style="transition-delay: ${idx * 50}ms">${letter}</span>`)
     .join("");
 });
+
