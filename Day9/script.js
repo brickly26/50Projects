54f3db7d0ecc65317a3cafa8c183fd22c03b4c74
brickly26 <sabburak26@gmail.com>
2022-09-19 19:42:08 -0400

    sounds play when button is pressed and all sounds are stopped when a new button is pressed

--- Day9/script.js
+++ Day9/script.js
@@ -6,7 +6,19 @@ sounds.forEach(sound => {
 
   btn.innerText = sound
 
-  document.getElementById('buttons').appendChild(btn)
+  btn.addEventListener('click', () => {
+    stopSongs()
 
-  btn.addEventListener
+    document.getElementById(sound).play()
+  })
+
+  document.getElementById('buttons').appendChild(btn)
 })
+
+function stopSongs() {
+  sounds.forEach(sound => {
+    const song = document.getElementById(sound)
+    song.pause()
+    song.currentTime = 0;
+  })
+}
