8fc917ce9e364d3a31418ebd3877e1fa22b20fff
brickly26 <sabburak26@gmail.com>
2022-09-22 22:10:05 -0400

    removed console.log

--- Day12/script.js
+++ Day12/script.js
@@ -1,7 +1,5 @@
 const buttons = document.querySelectorAll('.faq-toggle')
 
-console.log(buttons)
-
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
     button.parentElement.classList.toggle('active')
